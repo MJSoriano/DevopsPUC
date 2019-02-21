b6d14f15c024c7904bf772614083410adf8d06df
Mateus José Soriano <1136321@sga.pucminas.br>
2019-02-21 20:57:43 -0300

    Atualizando APP

--- AulaDevops/src/main/java/jar/App.java
+++ AulaDevops/src/main/java/jar/App.java
@@ -1,12 +1,88 @@
 package jar;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
+/**
+ * Hello world!
+ *
+ */
 public class App {
-    public static void main(String[] args) {       
+    public static void main(String[] args) {
+        Random r = new Random();
 
+        List<Integer> pesos = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        List<String> valores = Arrays.asList("2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", "As");
+        List<String> naipes = Arrays.asList("D", "H", "S", "C");
+        List<Carta> mao1 = new ArrayList<Carta>();
+        List<Carta> mao2 = new ArrayList<Carta>();
+
+        Collections.shuffle(pesos);
+
+        // Gera Mao1 de forma aleatoria
+        for(int i=0; i < 5; i++) {
+            Integer peso = pesos.get(i);
+            String valor = valores.get(peso);
+            mao1.add(new Carta(peso, valor, naipes.get(r.nextInt(naipes.size()))));
+        }
+
+        Collections.shuffle(pesos);
+
+        // Gera Mao2 de forma aleatoria
+        for(int i=0; i < 5; i++) {
+            Integer peso = pesos.get(i);
+            String valor = valores.get(peso);
+            mao2.add(new Carta(peso, valor, naipes.get(r.nextInt(naipes.size()))));
+        }
+
+        // Ordena a Mao1 e Mao2
+        ordenarMao(mao1);
+        ordenarMao(mao2);
+
+        System.out.println("--------------------- Mao 1 ---------------------");
+        for (Carta c : mao1) {
+            System.out.println(c);
+        }
+
+        System.out.println("--------------------- Mao 2 ---------------------");
+        for (Carta c : mao2) {
+            System.out.println(c);
+        }
+
+        imprimeMaoTeste();
+
+        System.out.println("--------------------- Verifica Straight Flush Mao 1  ---------------------");
+        System.out.println(verificaStraightFlush(mao1));
+
+        System.out.println("--------------------- Verifica Straight Flush Mao 2  ---------------------");
+        System.out.println(verificaStraightFlush(mao2));
+
+    }
+
+    public static void imprimeMaoTeste() {
+        List<Carta> maoTeste = new ArrayList<Carta>();
+        maoTeste.add(new Carta(8,"10","H"));
+        maoTeste.add(new Carta(9,"Vale","H"));
+        maoTeste.add(new Carta(10,"Data","H"));
+        maoTeste.add(new Carta(11,"Rei","H"));
+        maoTeste.add(new Carta(12,"As","H"));
+
+        ordenarMao(maoTeste);
+
+        System.out.println("--------------------- Mao Teste ---------------------");
+        for (Carta c : maoTeste) {
+            System.out.println(c);
+        }
+
+        /* System.out.println("--------------------- Verifica Straight Flush Mao Teste  ---------------------");
+        System.out.println(verificaStraightFlush(maoTeste)); */
+
+        System.out.println("--------------------- Verifica Royal Flush Mao Teste  ---------------------");
+        System.out.println(verificaRoyalFlush(maoTeste));
     }
 
     // Ordena a Mao do jogador pelo Peso da carta
@@ -40,4 +116,5 @@ public class App {
         }
         return false;
     }
+
 }

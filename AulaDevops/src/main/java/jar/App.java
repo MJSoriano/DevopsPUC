db60c3f97a41ff1423a3f02503fdb8f38643c6e0
Mateus José Soriano <1136321@sga.pucminas.br>
2019-02-21 20:21:00 -0300

    Correção do App

--- AulaDevops/src/main/java/jar/App.java
+++ AulaDevops/src/main/java/jar/App.java
@@ -1,39 +1,12 @@
 package jar;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args) {       
 
-    }
-
-    public static void imprimeMaoTeste() {
-        List<Carta> maoTeste = new ArrayList<Carta>();
-        maoTeste.add(new Carta(8,"10","H"));
-        maoTeste.add(new Carta(9,"Vale","H"));
-        maoTeste.add(new Carta(10,"Data","H"));
-        maoTeste.add(new Carta(11,"Rei","H"));
-        maoTeste.add(new Carta(12,"As","H"));
-
-        ordenarMao(maoTeste);
-
-        System.out.println("--------------------- Mao Teste ---------------------");
-        for (Carta c : maoTeste) {
-            System.out.println(c);
-        }
-
-        /* System.out.println("--------------------- Verifica Straight Flush Mao Teste  ---------------------");
-        System.out.println(verificaStraightFlush(maoTeste)); */
-
-        System.out.println("--------------------- Verifica Royal Flush Mao Teste  ---------------------");
-        System.out.println(verificaRoyalFlush(maoTeste));
     }
 
     // Ordena a Mao do jogador pelo Peso da carta
@@ -67,5 +40,4 @@ public class App {
         }
         return false;
     }
-
 }
